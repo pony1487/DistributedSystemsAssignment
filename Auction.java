b671296877af3a4ce67584e51a33aeef26d8739b
Ronan <connollyro@gmail.com>
2018-11-15 12:33:23 +0000

    Copied Chat files and it works

--- Auction.java
+++ Auction.java
@@ -1,103 +1,140 @@
-
-
-import java.io.IOException;
-import java.io.*;
 import java.net.*;
-import java.util.*;
+import java.io.*;
 
 
+public class Auction implements Runnable
+{
 
+    // Array of clients	
+    private AuctionThread clients[] = new AuctionThread[50];
+    private ServerSocket server = null;
+    private Thread       thread = null;
+    private int clientCount = 0;
 
-public class Auction {
-    /*
-    One item per auction: Expand it later
-     */
-    private ArrayList<ClientHandler> listOfClientHandlers = new ArrayList<ClientHandler>();
-    public ArrayList<User> users = new ArrayList<User>();
-    public Item item;
-    public ArrayList<Bid>  bids = new ArrayList<Bid>();
-
-    private ServerSocket servSock;
-    private final int PORT = 1234;
-
-
-
-    private final int ONE_SECOND = 1000;
-    private final int MAX_BID_TIME = ONE_SECOND * 60;
-
-    private final long createdMillis;
-    private long nowMillis;
-
-    public Auction()
+    public Auction(int port)
     {
-        this.item = new Item("Stratocaster","Fender guitar",999.99f);
+        try {
 
-        createdMillis = System.currentTimeMillis();
+            System.out.println("Binding to port " + port + ", please wait  ...");
+            server = new ServerSocket(port);
+            System.out.println("Server started: " + server.getInetAddress());
+            start();
+        }
+        catch(IOException ioe)
+        {
+            System.out.println("Can not bind to port " + port + ": " + ioe.getMessage());
 
+        }
     }
 
-    private void runAuction()
+    public void run()
     {
-        System.out.println("Opening port...\n");
-        try
+        while (thread != null)
         {
-            servSock = new ServerSocket(PORT);      //Step 1.
-        }
-        catch(IOException ioEx)
-        {
-            System.out.println("Unable to attach to port!");
-            System.exit(1);
-        }
+            try{
 
+                System.out.println("Waiting for a client ...");
+                addThread(server.accept());
 
-        while (true) {
-            //Wait for client..
-            try {
-                Socket client = servSock.accept();
+                int pause = (int)(Math.random()*3000);
+                Thread.sleep(pause);
 
-                System.out.println("\nNew client accepted.\n");
-
-                //Create a thread to handle communication with
-                //this client and pass the constructor for this
-                //thread a reference to the relevant socket...
-                ClientHandler handler = new ClientHandler(client,this);
-                listOfClientHandlers.add(handler);
-
-                handler.start();//As usual, this method calls run.
-
-            }catch(IOException ioEx)
-            {
-                System.out.println("Unable to attach to port!");
-                System.exit(1);
+            }
+            catch(IOException ioe){
+                System.out.println("Server accept error: " + ioe);
+                stop();
+            }
+            catch (InterruptedException e){
+                System.out.println(e);
             }
         }
     }
 
-    public void notifyClientsOfMaxBid(){
-        //This works but requires client to press enter to receive a line from the server to get the update
-
-        for(int i = 0; i < listOfClientHandlers.size();i++){
-            listOfClientHandlers.get(i).displayCurrentMaxBid();
+    public void start()
+    {
+        if (thread == null) {
+            thread = new Thread(this);
+            thread.start();
         }
+    }
+
+    public void stop(){
+        thread = null;
+
+    }
+
+    private int findClient(int ID)
+    {
+        for (int i = 0; i < clientCount; i++)
+            if (clients[i].getID() == ID)
+                return i;
+        return -1;
+    }
+
+    public synchronized void broadcast(int ID, String input)
+    {
+        if (input.equals(".bye")){
+            clients[findClient(ID)].send(".bye");
+            remove(ID);
+        }
+        else
+            for (int i = 0; i < clientCount; i++){
+                if(clients[i].getID() != ID)
+                    clients[i].send(ID + ": " + input); // sends messages to clients
+            }
         notifyAll();
     }
-
-
-    public int getSecondCount()
+    public synchronized void remove(int ID)
     {
-        nowMillis = System.currentTimeMillis();
-        return (int)((nowMillis - this.createdMillis) / 1000);
+        int pos = findClient(ID);
+        if (pos >= 0){
+            AuctionThread toTerminate = clients[pos];
+            System.out.println("Removing client thread " + ID + " at " + pos);
+
+            if (pos < clientCount-1)
+                for (int i = pos+1; i < clientCount; i++)
+                    clients[i-1] = clients[i];
+            clientCount--;
+
+            try{
+                toTerminate.close();
+            }
+            catch(IOException ioe)
+            {
+                System.out.println("Error closing thread: " + ioe);
+            }
+            toTerminate = null;
+            System.out.println("Client " + pos + " removed");
+            notifyAll();
+        }
     }
 
-    public void endAuction()
+    private void addThread(Socket socket)
     {
+        if (clientCount < clients.length){
 
-        System.exit(1);
+            System.out.println("Client accepted: " + socket);
+            clients[clientCount] = new AuctionThread(this, socket);
+            try{
+                clients[clientCount].open();
+                clients[clientCount].start();
+                clientCount++;
+            }
+            catch(IOException ioe){
+                System.out.println("Error opening thread: " + ioe);
+            }
+        }
+        else
+            System.out.println("Client refused: maximum " + clients.length + " reached.");
     }
 
 
-    public static void main(String[] args) {
-        Auction auction = new Auction();
-        auction.runAuction();
+    public static void main(String args[]) {
+        Auction server = null;
+        if (args.length != 1)
+            System.out.println("Usage: java Auction port");
+        else
+            server = new Auction(Integer.parseInt(args[0]));
     }
+
 }
